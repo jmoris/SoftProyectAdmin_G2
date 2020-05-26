314b1f2cf1e7e2621602d32a569210adcfb01768
Bastian <bastian.sepiulvedagarcia@gmail.com>
2020-05-26 19:05:59 -0400

    Avance T07: Controladores Backend

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -7,4 +7,139 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     //
+
+    public function crearUsuario(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'rut' => 'required',
+            'enrollment' => 'required',
+            'email' => 'required',
+            'name' => 'required|email',
+            'surname' => 'required',
+            'password' => 'required',
+            'role' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('post/create')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
+        $id = $request->input('id');
+        $rut = $request->input('rut');
+        $enrollment = $request->input('enrollment');
+        $email = $request->input('email');
+        $name = $request->input('name');
+        $surname = $request->input('surname');
+        $password = $request->input('password');
+        $role = $request->input('role');
+
+        $user = User::create(
+        ['id' => $id,
+        'rut' => $rut,
+        'enrollment' => $enrollment,
+        'email' => $email,
+        'name' => $name,
+        'surname' => $surname,
+        'password' => $password,
+        'role' => $role,
+        ]);
+
+        $user->save();
+    }
+
+    public function editarUsuario(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'rut' => 'required',
+            'enrollment' => 'required',
+            'email' => 'required',
+            'name' => 'required|email',
+            'surname' => 'required',
+            'password' => 'required',
+            'role' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('post/create')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
+        $id = $request->input('id');
+        $rut = $request->input('rut');
+        $enrollment = $request->input('enrollment');
+        $email = $request->input('email');
+        $name = $request->input('name');
+        $surname = $request->input('surname');
+        $password = $request->input('password');
+        $role = $request->input('role');
+
+        
+        $user = User::find($id);
+
+        $user->rut=$rut;
+        $user->enrollment = $enrollment;
+        $user->email = $email;
+        $user->name=$name;
+        $user->surname = $surname;
+        $user->password = $password;
+        $user->role = $role;
+
+        $user->save();
+    }
+
+    public function eliminarUsuario($idEstudiante)
+    {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'rut' => 'required',
+            'enrollment' => 'required',
+            'email' => 'required',
+            'name' => 'required|email',
+            'surname' => 'required',
+            'password' => 'required',
+            'role' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('post/create')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
+        $id = $request->input('id');
+        $user = User::find($id);
+        $user->delete();
+    }
+
+    /**
+     * Crear un curso 
+     */
+    public function crearCurso(Request $request)
+    {
+
+    }
+
+    /**
+     * Asignar un profesor a un curso
+     */
+    public function asignarProfesorACurso(Request $request)
+    {
+        
+
+    } 
+
+
+
+    
+    
+
+
+    
+
+
 }
--- backend/app/Http/Controllers/DocenteController.php
+++ backend/app/Http/Controllers/DocenteController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class DocenteController extends Controller
 {
-    //
+    /**
+     * Obtener todos los proyectos en los que participa un docente
+     * 
+     */
+    public function getProyectos($idDocente)
+    {
+
+    }
+
+    /**
+     * Eliminar un proyecto en el que participa un docente
+     */
+    public function eliminarProyecto($idProyecto)
+    {
+
+    }
+
+    
+
+
+
+
 }
