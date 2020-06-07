f7e743ad00a52c9afd51fe216bd6f788c70738ec
jmoris <jesusmoriis@hotmail.com>
2020-06-07 04:03:44 -0400

    T12: Se publico rutas para gestion de proyectos y se modifico pequeñas cosas

--- backend/app/RoleUser.php
+++ backend/app/RoleUser.php
@@ -9,4 +9,12 @@ class RoleUser extends Model
     public function projects(){
         return $this->belongsToMany(Project::class);
     }
+
+    public function user(){
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    public function role(){
+        return $this->hasOne(Role::class, 'id', 'role_id');
+    }
 }
