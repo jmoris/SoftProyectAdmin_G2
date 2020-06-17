1eabfc7c48a4224c84967d39d428926bf3d44c97
BastianIgnacio <bastian.sepulvedagarcia@gmail.com>
2020-06-17 05:35:09 -0400

    Avance del componente que gestiona un proyecto
    @jmoris

--- frontend/src/app/views/proyectos/project/project.component.spec.ts
+++ frontend/src/app/views/proyectos/project/project.component.spec.ts
@@ -2,8 +2,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectComponent } from './project.component';
 
-describe('AddProjectComponent', () => {
+describe('ProjectComponent', () => {
   let component: ProjectComponent;
+  let fixture: ComponentFixture<ProjectComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
--- frontend/src/app/views/proyectos/project/project.component.ts
+++ frontend/src/app/views/proyectos/project/project.component.ts
@@ -1,28 +1,51 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ProjectsService } from 'src/app/_services/projects.service';
+import { Component, OnInit } from '@angular/core';
+import { SharedAnimations } from 'src/app/shared/animations/shared-animations';
+import { echartStyles } from 'src/app/shared/echart-styles';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
+
 export class ProjectComponent implements OnInit {
+  
+  active = 1;
+  name="nombreProyecto";
+  chartPie1: any;
+  chartLineOption3: any;
+  products$: any;
+  
+  constructor() { }
 
+  ngOnInit() {
+    this.chartPie1 = {
+			...echartStyles.defaultOptions, ...{
+        legend: {
+          show: true,
+          bottom: 0,
+        },
+				series: [{
+          type: 'pie',
+          ...echartStyles.pieRing,
 
-  form: FormGroup;
-  hide = true;
+          label: echartStyles.pieLabelCenterHover,
+					data: [{
+						name: 'Exitosos',
+						value: 80,
+						itemStyle: {
+							color: '#663399',
+						}
+					}, {
+						name: 'Fallidos',
+						value: 20,
+						itemStyle: {
+							color: '#ced4da',
+						}
+					}]
+				}]
+			}
+    };
 
-
-  constructor(
-    private projectService: ProjectsService
-  ) {
-    
-  }
-
-  ngOnInit(): void {
-  }
-
-
+}
 }
