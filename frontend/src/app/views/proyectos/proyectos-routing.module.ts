ffc3ef3489ad86024b5c3e412fcefffdcc5ae769
BastianIgnacio <bastian.sepulvedagarcia@gmail.com>
2020-06-16 23:06:28 -0400

    Creación del modulo que gestiona un proyecto

--- frontend/src/app/views/proyectos/proyectos-routing.module.ts
+++ frontend/src/app/views/proyectos/proyectos-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GestionComponent } from './gestion/gestion.component';
+import { ProjectComponent } from './project/project.component';
 
 const routes: Routes = [
   {
     path: 'gestion',
     component: GestionComponent
+  },
+  {
+    path: 'gestion/proyecto',
+    component: ProjectComponent
   },
 ];
 
