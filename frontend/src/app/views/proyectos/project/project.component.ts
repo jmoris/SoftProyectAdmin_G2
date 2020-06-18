b5689ee317a458b92baa492085447e8ff0342447
BastianIgnacio <bastian.sepulvedagarcia@gmail.com>
2020-06-18 00:17:05 -0400

    Creación de los modal que gestionan un proyecto

--- frontend/src/app/views/proyectos/project/project.component.ts
+++ frontend/src/app/views/proyectos/project/project.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedAnimations } from 'src/app/shared/animations/shared-animations';
 import { echartStyles } from 'src/app/shared/echart-styles';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DataLayerService } from 'src/app/shared/services/data-layer.service';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-project',
@@ -15,37 +19,76 @@ export class ProjectComponent implements OnInit {
   chartPie1: any;
   chartLineOption3: any;
   products$: any;
+
+  items = ['Javascript', 'Typescript'];
+  autocompletes$;
+  tagsCtrl1 = new FormControl(this.items);
+
   
-  constructor() { }
+  constructor(
+    private modalService: NgbModal,
+    private dl: DataLayerService,
+  ) { }
+
+  public onSelect(item) {
+    console.log('tag selected: value is ' + item);
+  }
+
+  
 
   ngOnInit() {
     this.chartPie1 = {
-			...echartStyles.defaultOptions, ...{
+      ...echartStyles.defaultOptions, ...{
         legend: {
           show: true,
           bottom: 0,
         },
-				series: [{
+        series: [{
           type: 'pie',
           ...echartStyles.pieRing,
 
           label: echartStyles.pieLabelCenterHover,
-					data: [{
-						name: 'Exitosos',
-						value: 80,
-						itemStyle: {
-							color: '#663399',
-						}
-					}, {
-						name: 'Fallidos',
-						value: 20,
-						itemStyle: {
-							color: '#ced4da',
-						}
-					}]
-				}]
-			}
+          data: [{
+            name: 'Exitosos',
+            value: 80,
+            itemStyle: {
+              color: '#663399',
+            }
+          }, {
+            name: 'Fallidos',
+            value: 20,
+            itemStyle: {
+              color: '#ced4da',
+            }
+          }]
+        }]
+      }
     };
+  }
 
-}
+  addUserRequeriment(modal, event)
+  {
+    event.target.parentElement.parentElement.blur();
+    this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title', centered: true, size: 'lg' });
+       
+  }
+
+  addSoftwareRequeriment(modal, event)
+  {
+    event.target.parentElement.parentElement.blur();
+    this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title', centered: true, size: 'lg' });
+       
+
+  }
+
+  addTestCase(modal, event)
+  {
+    event.target.parentElement.parentElement.blur();
+    this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title', centered: true, size: 'lg' });
+       
+
+  }
+
+
+
 }
