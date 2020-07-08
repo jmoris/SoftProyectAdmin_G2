9f5b21413c1444427380d84aac77a8a8f40673ff
Thitsugaya1 <nikho38@gmail.com>
2020-07-08 16:40:53 -0400

    T16: Permitir Ediciones de Información (Avance)
    Se creo la parte gráfica (Front), pero hubo problemas con la integración al proyecto. Ademas se deja creado y ubicado el botón que permitirá abrir el modal de edición

---
+++ frontend/src/app/views/proyectos/edit-project/edit-project.component.ts
@@ -0,0 +1,180 @@
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AddProjectComponent } from '../add-project/add-project.component';
+import { ProjectsService } from 'src/app/_services/projects.service';
+import { UsuariosService } from 'src/app/_services/usuarios.service';
+
+@Component({
+  selector: 'app-edit-project',
+  templateUrl: './edit-project.component.html',
+  styleUrls: ['./edit-project.component.css']
+})
+export class EditProjectComponent implements OnInit {
+
+  form: FormGroup;
+  seleccionarForm: FormGroup;
+  hide = true;
+  isCompleted: boolean;
+  asignarForm: FormGroup;
+  currentYear: number = new Date().getFullYear(); ;
+  roles: any = [];
+  estudiantes: any = [];
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
+  dataSource2: MatTableDataSource<any> = new MatTableDataSource<any>();
+  isDataLoading: boolean;
+  selected = [];
+  displayedColumns: string[] = [ 'select', 'name', 'surname', 'email'];
+  displayedColumns2: string[] = [ 'name', 'surname', 'email', 'rol'];
+  ColumnMode = ColumnMode;
+  SelectionType = SelectionType;
+  selection = new SelectionModel<any>(true, []);
+
+  @ViewChild('sort1') sort: MatSort;
+  @ViewChild('sort2') sort2: MatSort;
+  @ViewChild('paginator') paginator: MatPaginator;
+  @ViewChild('paginator2') paginator2: MatPaginator;
+
+  constructor(
+    public dialogRef: MatDialogRef<AddProjectComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: String,
+    private projectService: ProjectsService,
+    private userService: UsuariosService,
+    private formBuilder: FormBuilder
+  ) {
+    this.userService.getStudents().subscribe((data) => {
+        if (!data) {
+            return;
+          }
+          this.estudiantes = data;
+          this.dataSource.data = this.estudiantes;
+          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
+          this.isDataLoading = false;
+    });
+    this.projectService.getRoles().subscribe((data) => {
+        this.roles = data;
+    });
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      year: new FormControl(this.currentYear, [Validators.required]),
+      semester: new FormControl('', [Validators.required]),
+    });
+    this.seleccionarForm = new FormGroup({
+        usuarios: new FormArray([], [Validators.required, Validators.minLength(1)])
+    });
+    this.asignarForm = new FormGroup({
+        usuarios: new FormArray([], [Validators.required, Validators.minLength(this.dataSource2.data.length)])
+      });
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  onCloseConfirm() {
+    if (this.form.invalid) {
+      (<any>Object).values(this.form.controls).forEach(control => {
+        control.markAsTouched();
+      });
+      return;
+    }
+    const projectData = this.form.value;
+    console.log('Info name: ' + projectData.name);
+    this.projectService.insert(projectData).subscribe({
+      next: result => {
+        console.log(result);
+
+      },
+      error: result => { }
+    });
+  }
+
+  onCloseCancel() {
+    this.dialogRef.close('Cancel');
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.form.get(controlName).hasError(errorName);
+  }
+
+  onStep1Next(e) {}
+  onStep2Next(e) {
+    this.dataSource2.data = this.selection.selected;
+    this.dataSource2.sort = this.sort2;
+    this.dataSource2.paginator = this.paginator2;
+    this.asignarForm = new FormGroup({
+        usuarios: new FormArray([], [Validators.required, Validators.minLength(this.dataSource2.data.length)])
+      });
+  }
+
+  onComplete(e) {
+      const frm = this.form.value;
+      frm.students = this.asignarForm.value.usuarios;
+      this.projectService.insertComplete(frm).subscribe((data) => {
+          console.log(data);
+          this.dialogRef.close('Confirm');
+      });
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected == numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    if (this.isAllSelected()) {
+        this.seleccionarForm.controls.usuarios.setValue([]);
+        this.selection.clear();
+    } else {
+        const usersControl = <FormArray>this.seleccionarForm.controls.usuarios;
+        this.dataSource.data.forEach(row => this.selection.select(row));
+        this.estudiantes.forEach(element => {
+            usersControl.push(this.formBuilder.group({user_id: element.id}));
+        });
+
+    }
+  }
+
+  seleccionar(row) {
+    const usersControl = <FormArray>this.seleccionarForm.controls.usuarios;
+    const selected = this.selection.isSelected(row);
+    if (selected) {
+        usersControl.push(this.formBuilder.group({user_id: row.id}));
+    } else {
+        usersControl.removeAt(usersControl.value.findIndex(student => student.id === row.id));
+    }
+  }
+
+  searchById(id) {
+    const usuarios: any = this.seleccionarForm.controls.usuarios.value;
+    let search: any = null;
+    usuarios.forEach(element => {
+        // tslint:disable-next-line: triple-equals
+        if (element.user_id == id) {
+            search = element;
+        }
+    });
+    return search; ;
+  }
+
+    selectRol(user, rol) {
+        const searched = this.searchById(user.id);
+        searched.role_id = rol.id;
+        const usersControl = <FormArray>this.asignarForm.controls.usuarios;
+        usersControl.push(this.formBuilder.group({
+            user_id: new FormControl(searched.user_id, [Validators.required]),
+            role_id: new FormControl(searched.role_id, [Validators.required])
+        }));
+    }
+
+}
--- frontend/src/app/views/proyectos/project/project.component.ts
+++ frontend/src/app/views/proyectos/project/project.component.ts
@@ -3,9 +3,14 @@ import { SharedAnimations } from 'src/app/shared/animations/shared-animations';
 import { echartStyles } from 'src/app/shared/echart-styles';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DataLayerService } from 'src/app/shared/services/data-layer.service';
-import { FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProjectsService } from 'src/app/_services/projects.service';
+import { EditProjectComponent } from '../edit-project/edit-project.component';
+import { ToastrService } from 'ngx-toastr';
+import { MatDialog } from '@angular/material/dialog';
+import { NotifierService } from 'angular-notifier';
+import { AddProjectComponent } from '../add-project/add-project.component';
 
 
 @Component({
@@ -15,36 +20,40 @@ import { ProjectsService } from 'src/app/_services/projects.service';
 })
 
 export class ProjectComponent implements OnInit {
+  private readonly notifier: NotifierService;
 
   active = 1;
-  name="nombreProyecto";
+  name = 'nombreProyecto';
   chartPie1: any;
   chartLineOption3: any;
   products$: any;
 
   items = ['Javascript', 'Typescript'];
-  autocompletes$;
+  autocompletes$: any;
   tagsCtrl1 = new FormControl(this.items);
-    id : any;
-proyecto : any;
-equipo:any = [];
+    id: any;
+proyecto: any;
+equipo: any = [];
 
 
-userRequerimentForm : FormGroup;
-softwareRequerimentForm : FormGroup;
-testCaseForm : FormGroup;
+userRequerimentForm: FormGroup;
+softwareRequerimentForm: FormGroup;
+testCaseForm: FormGroup;
+  dialogResult: any;
 
 
 
-
-  constructor
-  (
+  constructor (
     private route: ActivatedRoute,
     private modalService: NgbModal,
     private dl: DataLayerService,
-    private proyectoService : ProjectsService
-  )
-  {
+    private proyectoService: ProjectsService,
+    private toastr: ToastrService,
+    private fb: FormBuilder,
+    private dialog: MatDialog,
+    private router: Router,
+    notifierService: NotifierService) {
+    this.notifier = notifierService;
     this.id = this.route.snapshot.params['id'];
     proyectoService.get(this.id).subscribe((data) => {
         this.proyecto = data[0];
@@ -119,43 +128,70 @@ testCaseForm : FormGroup;
     };
   }
 
-  addUserRequeriment(modal, event)
-  {
+  addUserRequeriment(modal, event) {
+    event.target.parentElement.parentElement.blur();
+    this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title', centered: true, size: 'lg' });
+
+  }
+  openAddDialog() {
+    const dialogRef = this.dialog.open(AddProjectComponent, {
+      width: '850px',
+      data: 'This text is passed into the dialog',
+      disableClose: true,
+      autoFocus: true
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog closed: ${result}`);
+      this.dialogResult = result;
+      // tslint:disable-next-line: triple-equals
+      if (result == 'Confirm') {
+          this.toastr.success('Proyecto agregado exitosamente', 'Notificación', { timeOut: 3000 });
+      }
+  });
+  }
+
+  openEditDialog(): void {
+      const dialogRef = this.dialog.open(EditProjectComponent, {
+      width: '850px',
+      data: 'This text is passed into the dialog',
+      disableClose: true,
+      autoFocus: true
+  });
+  dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog closed: ${result}`);
+      this.dialogResult = result;
+      // tslint:disable-next-line: triple-equals
+      if (result == 'Confirm') {
+          this.toastr.success('Proyecto editado exitosamente', 'Notificación', { timeOut: 3000 });
+      }
+  });
+  }
+
+  addSoftwareRequeriment(modal, event) {
+    event.target.parentElement.parentElement.blur();
+    this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title', centered: true, size: 'lg' });
+  }
+
+  addTestCase(modal, event) {
     event.target.parentElement.parentElement.blur();
     this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title', centered: true, size: 'lg' });
 
   }
 
-  addSoftwareRequeriment(modal, event)
-  {
-    event.target.parentElement.parentElement.blur();
-    this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title', centered: true, size: 'lg' });
-  }
 
-  addTestCase(modal, event)
-  {
-    event.target.parentElement.parentElement.blur();
-    this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title', centered: true, size: 'lg' });
-
-  }
-
-
-  imprimirtc()
-  {
+  imprimirtc() {
     console.log(this.testCaseForm.value);
   }
 
-  imprimirur()
-  {
+  imprimirur() {
     console.log(this.userRequerimentForm.value);
 
   }
 
-  imprimirsr()
-  {
+  imprimirsr() {
     console.log(this.softwareRequerimentForm.value);
   }
- 
+
 
 
 }
