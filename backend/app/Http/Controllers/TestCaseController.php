f76b147964b710f7b19bbc7ce1d49a061fed5883
jmoris <jesusmoriis@hotmail.com>
2020-07-22 16:13:40 -0400

    Avance T22: Se implemento controlador y front casos de prueba
    @BastianIgnacio se implemento el controlador, las rutas y la carga de la tabla con los casos de uso.

--- backend/app/Http/Controllers/TestCaseController.php
+++ backend/app/Http/Controllers/TestCaseController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\TestCase;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TestCaseController extends Controller
 {
@@ -11,9 +13,17 @@ class TestCaseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $ur = TestCase::where('project_id', $request->project_id)->get();
+        return response()->json($ur);
+    }
+
+    public function getNextId($id){
+        $next_id = TestCase::where('project_id', $id)->orderBy('internalId', 'desc')->first();
+        if($next_id==null)
+            return response()->json(['next_id' => 1]);
+        return response()->json(['next_id' => $next_id->internalId + 1]);
     }
 
     /**
@@ -24,7 +34,31 @@ class TestCaseController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'description' => 'required',
+            'acceptableResult' => 'required',
+            'optimumResult' => 'required',
+            'status' => 'required',
+            'project_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+
+        $testc = new TestCase();
+        $testc->internalId = TestCase::where('project_id', $request->project_id)->count() + 1;
+        $testc->description = $request->description;
+        $testc->acceptableResult = $request->acceptableResult;
+        $testc->optimumResult = $request->optimumResult;
+        $testc->status = $request->status;
+        $testc->project_id = $request->project_id;
+        $testc->save();
+
+        return response()->json([
+            'success' => true,
+            'msg' => 'El caso de uso fue agregado exitosamente.'
+        ]);
     }
 
     /**
@@ -35,7 +69,15 @@ class TestCaseController extends Controller
      */
     public function show($id)
     {
-        //
+        $tc = TestCase::find($id);
+        if($tc){
+            return response()->json($tc);
+        }else{
+            return response()->json([
+                'success' => false,
+                'msg' => 'El caso de uso no existe en la base de datos.'
+            ]);
+        }
     }
 
     /**
@@ -47,7 +89,31 @@ class TestCaseController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'description' => 'required',
+            'acceptableResult' => 'required',
+            'optimumResult' => 'required',
+            'status' => 'required',
+            'project_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+
+        $testc = TestCase::find($id);
+        $testc->internalId = TestCase::where('project_id', $request->project_id)->count() + 1;
+        $testc->description = $request->description;
+        $testc->acceptableResult = $request->acceptableResult;
+        $testc->optimumResult = $request->optimumResult;
+        $testc->status = $request->status;
+        $testc->project_id = $request->project_id;
+        $testc->save();
+
+        return response()->json([
+            'success' => true,
+            'msg' => 'El caso de uso fue modificadp exitosamente.'
+        ]);
     }
 
     /**
@@ -58,6 +124,17 @@ class TestCaseController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $tc = TestCase::find($id);
+        if($tc){
+            $tc->delete();
+            return response()->json([
+                'success' => true,
+                'msg' => 'Caso de uso eliminado correctamente.'
+            ]);
+        }
+        return response()->json([
+            'success' => false,
+            'msg' => 'No se puede eliminar el caso de uso.'
+        ]);
     }
 }
